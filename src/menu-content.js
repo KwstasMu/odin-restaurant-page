70b9c091b00e14f16fe9a01a67c1d2c2e89e1915
KwstasMu <kwstasmu@hotmail.com>
2025-10-23 17:27:14 +0300

    fix menu to be an actuall menu and not a recipe book

--- src/menu-content.js
+++ src/menu-content.js
@@ -8,54 +8,47 @@ export function menuContent() {
     const cardsDiv = document.createElement('div');
     cardsDiv.classList.add('cards-div');
     
-    for (let i = 1; i <= 6; i++) {
+    for (let i = 1; i <= 9; i++) {
     const card = document.createElement('div');
     card.classList.add('card');
 
     const header = document.createElement('h3');
     header.classList.add('card-header');
-    header.textContent = 'Recipe ' + i;
+    header.textContent = 'Menu item ' + i;
 
-    const hr1 = document.createElement('hr');
-    hr1.style.width = '100%'
-    hr1.style.margin = '0'
+    const price = document.createElement('p');
+    price.classList.add('menu-item-price');
+    price.textContent = '$' + ((Math.random() * 50) + 1 + Math.random()).toFixed(2);
+
+    // const hr1 = document.createElement('hr');
+    // hr1.style.width = '100%'
+    // hr1.style.margin = '0'
 
     card.insertAdjacentElement('beforeend', header);
-    card.insertAdjacentElement('beforeend', hr1)
+    card.insertAdjacentElement('beforeend', price)
+    // card.insertAdjacentElement('beforeend', hr1)
 
-    const ingredientsList = document.createElement('ul');
-    ingredientsList.classList.add('ingredients-list');
+    const menuItemImg = document.createElement('img');
+    menuItemImg.classList.add('menu-item-img');
 
-    const ingredientsHeader = document.createElement('h4');
-    ingredientsHeader.classList.add('ingredients-header');
-    ingredientsHeader.textContent = 'Ingredients'
+    card.insertAdjacentElement('beforeend', menuItemImg);
 
-    for (let i = 1; i <= 6; i++) {
-        const ingredient = document.createElement('li');
-        ingredient.classList.add('ingredient' + i);
-        ingredient.textContent = 'Ingredient ' + i + ': Some ammount';
-        ingredientsList.insertAdjacentElement('beforeend', ingredient);
-    }
+    const menuItemDescriptionDiv = document.createElement('div');
+    menuItemDescriptionDiv.classList.add('menu-item-description-div');
 
-    card.insertAdjacentElement('beforeend', ingredientsHeader);
-    card.insertAdjacentElement('beforeend', ingredientsList);
+    const menuItemDescription = document.createElement('h4');
+    menuItemDescription.classList.add('menu-item-description');
+    menuItemDescription.textContent = lorem.generateSentences(3);
+
+    menuItemDescriptionDiv.insertAdjacentElement('beforeend', menuItemDescription);
     
-    const hr2 = document.createElement('hr');
-    hr2.style.width = '100%'
-    hr2.style.margin = '0'
+    // const hr2 = document.createElement('hr');
+    // hr2.style.width = '100%'
+    // hr2.style.margin = '0'
 
-    card.insertAdjacentElement('beforeend', hr2);
+    // card.insertAdjacentElement('beforeend', hr2);
 
-    const instructionsHeader = document.createElement('h4');
-    instructionsHeader.classList.add('instructions-header');
-    instructionsHeader.textContent = 'Instructions';
-
-    const instructions = document.createElement('p');
-    instructions.classList.add('recipe-instructions');
-    instructions.textContent = lorem.generateSentences(3);
-   
-    card.insertAdjacentElement('beforeend', instructionsHeader);
-    card.insertAdjacentElement('beforeend', instructions);
+    card.insertAdjacentElement('beforeend', menuItemDescriptionDiv);
 
     cardsDiv.insertAdjacentElement('beforeend', card);
     
